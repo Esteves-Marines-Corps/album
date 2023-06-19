5546012bbf6ce9260b0525f8762c64568b79964a
Esteves-Marines-Corps <luguilher@hotmail.com>
2023-06-19 16:44:44 -0300

    Criando o arquivo .js e codando para alternar entre as abas

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+function mostraGuilherme(el) {
+    document.getElementById(el).style.display = 'block';
+
+    if (display = 'block') {
+        document.getElementById('Home').style.display = 'none';
+        document.getElementById('Daniela').style.display = 'none';
+        document.getElementById('Davi').style.display = 'none';
+    }
+}
+
+function mostraDaniela(el) {
+    document.getElementById(el).style.display = 'block';
+
+    if (display = 'block') {
+        document.getElementById('Home').style.display = 'none';
+        document.getElementById('Guilherme').style.display = 'none';
+        document.getElementById('Davi').style.display = 'none';
+    }
+}
+
+function mostraDavi(el) {
+    document.getElementById(el).style.display = 'block';
+
+    if (display = 'block') {
+        document.getElementById('Home').style.display = 'none';
+        document.getElementById('Guilherme').style.display = 'none';
+        document.getElementById('Daniela').style.display = 'none';
+    }
+}
